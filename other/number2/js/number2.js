d61dd8e17666287eead9797f54a182bbbd71923f
edodo2 <kjhkjhkjhkjhkjhkjh@gmail.com>
2024-04-07 02:04:33 +0900

    number2

---
+++ other/number2/js/number2.js
@@ -0,0 +1,86 @@
+const base = "https://twitter.com/intent/tweet?ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Eshare%7Ctwgr%5E&url=https%3A%2F%2Fedodo2.github.io%2Fother%2Fnumber2%2Fstart.html&hashtags=1からNまで押せたね_edodo2&text=";
+const RenderHtmlApp = {
+    data() {
+        return {
+            twibun: base,
+            load: false,
+            now: "",
+            nums: [],
+            hoju: [],
+            hoju2: [],
+            hojuCount: 0,
+            timer: null,
+            time: "",
+            startTime: 0,
+        }
+    },
+    methods: {
+        firstSet() {
+            this.now = 1;
+            this.nums = this.makeList(1);
+            this.hoju = this.makeList(17);
+            this.hoju2 = this.makeList(33);
+        },
+        makeList(st) {
+            let list = [];
+            let numslength = 0;
+            while (numslength < 16) {
+                let num = Math.floor(Math.random() * 16) + st;
+                if (!list.includes(num)) {
+                    numslength = list.push(num);
+                }
+            }
+            return list;
+        },
+        push(index) {
+            if (this.nums[index] == this.now) {
+                let n = this.now++;
+                this.nums[index] = this.hoju.pop();
+                if (this.hoju.length < 1 && n % 16 == 0) {
+                    this.hoju = this.hoju2;
+                    this.hojuCount++;
+                    this.hoju2 = this.makeList(16 * (this.hojuCount + 2) + 1);
+                }
+            }
+        },
+        start() {
+            this.firstSet();
+            this.startTime = new Date().getTime();
+            this.timer = setInterval(this.timeMane, 50);
+        },
+        timeMane() {
+            this.time = 30000 - (new Date().getTime() - this.startTime);
+            if (this.time < 0) {
+                this.end();
+            }
+        },
+        end() {
+            clearInterval(this.timer);
+            this.timer = null;
+            this.twibun = base + "N=" + (this.now - 1);
+            modal.show();
+        },
+        reset() {
+            this.hojuCount = 0;
+            this.nums = [];
+            this.hoju = [];
+            this.hoju2 = [];
+            modal.hide();
+            setTimeout(this.start, 1000);
+        }
+    },
+    mounted() {
+        window.onload = () => {
+            document.getElementById('loading').classList.remove("d-none");
+            this.load = true;
+            setTimeout(this.start, 1000);
+        }
+    }
+};
+const app = Vue.createApp(RenderHtmlApp);
+
+const vm = app.mount('#number2');
+
+const modal = new bootstrap.Modal(document.getElementById('endmodal'), {
+    keyboard: false
+});
